ec96966f12a3af4ea6ce30c69df73e81b755e7f0
Michael Novotny <manovotny@gmail.com>
2014-09-09 12:01:35 -0500

    Cleans up the comments on the WP-CLI script.

--- wptest-cli-install.sh
+++ wptest-cli-install.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 #
-# wptest.io Quick Install Script
+# WP Test - WP-CLI Quick Install Script
 # http://wptest.io/
 #
 # Note: This script assumes you have wp-cli installed.
-##########################################################
+#####################################################################################
 
-# Get WordPress Path
+# Ask user where WordPress is installed.
 printf "Please provide the local path to your WordPress install: "
 read WPPATH
 
-# Import WP TESTS
+# Import WP Test data.
 cd $WPPATH
 curl -OL https://raw.githubusercontent.com/manovotny/wptest/master/wptest.xml
 wp import wptest.xml --authors=create
