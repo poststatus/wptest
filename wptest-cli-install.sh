79bde066cc788041c833b535ff1c0b8ab6a7848e
jeffmatson <jeff@jeffmatson.net>
2016-09-30 02:17:03 -0400

    Added flag functionality and --allow-root flag support.

--- wptest-cli-install.sh
+++ wptest-cli-install.sh
@@ -6,6 +6,21 @@
 # Note: This script assumes you have wp-cli installed.
 #####################################################################################
 
+# Check for flag usage.
+# If more flags are added here, this could probably be simplified a bit better.
+case "$1" in
+    --allow-root)
+        ALLOWROOT=$1
+        ;;
+    "")
+        ALLOWROOT=""
+        ;;
+    *)
+    printf "Invalid flag provided. Allowed flags: --allow-root.\n"
+    exit
+    ;;
+esac
+
 # Ask user where WordPress is installed.
 printf "Please provide the local path to your WordPress install: "
 read WPPATH
