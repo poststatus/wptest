66b82abfd066e3656c583e0dd90889ddf40049f6
jeffmatson <jeff@jeffmatson.net>
2016-09-30 02:36:49 -0400

    Whoops again. Check for cURL Install the WP Importer

--- wptest-cli-install.sh
+++ wptest-cli-install.sh
@@ -9,16 +9,16 @@
 # Check for flag usage.
 # If more flags are added here, this could probably be simplified a bit better.
 case "$1" in
-    --allow-root)
+    "--allow-root")
         ALLOWROOT=$1
         ;;
     "")
         ALLOWROOT=""
         ;;
     *)
-    printf "Invalid flag provided. Allowed flags: --allow-root.\n"
-    exit
-    ;;
+        printf "Invalid flag provided. Allowed flags: --allow-root.\n"
+        exit 1
+        ;;
 esac
 
 # Ask user where WordPress is installed.
@@ -32,14 +32,23 @@ read USELOCAL
 # Move to the WordPress installation location.
 cd $WPPATH
 
+# Include the WordPress importer
+wp plugin install wordpress-importer --activate $ALLOWROOT
+
 # Check if a custom path is being used.
-if [ "$USELOCAL" == 'y' ]; then
+if [ "$USELOCAL" = 'y' ]; then
     # Get the path to the xml file.
     printf "Please provide the absolute local path to your xml file: "
     read XMLPATH
     # Import the local file.
     wp import $XMLPATH --authors=create $ALLOWROOT
 else
+    # Check for cURL.
+    if ! command -v curl > /dev/null 2>&1; then
+        printf "cURL is not installed or accessible. Install cURL to use remote xml files.\n"
+        exit
+    fi
+
     # Get the xml file.
     curl -OL https://raw.githubusercontent.com/poststatus/wptest/master/wptest.xml
     # Import the file, then delete it.
